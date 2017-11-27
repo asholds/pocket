f20fad2f31d9ff1bf8a558759835981fe4aea5a0
asholds <asholds@gmail.com>
2017-11-27 13:15:22 -0500

    refresh button

--- javascripts/pocket.js
+++ javascripts/pocket.js
@@ -4,6 +4,9 @@ var Pocket = function(){
 	var jsonUrl = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fgetpocket.com%2Fusers%2Fasholds%2Ffeed%2Funread"
 
 	var getUnread = function(){
+		unreadList = [];
+		$('#links').html("");
+
 		$.get(jsonUrl, function(data) {
 		    if(data.status == "ok"){
 		    	$.each(data.items,function(i,v){
@@ -27,12 +30,8 @@ var Pocket = function(){
 	var openSelected = function(){
 		var selected = $('.selected');
 		$.each(selected,function(i,v){
-			//console.log($(v));
 			window.open($(v).attr('linkurl'));
 		})
-		//$.each(unreadList, function(i,v){
-			//window.open(v.link)
-		//});
 	}
 
 	return {
